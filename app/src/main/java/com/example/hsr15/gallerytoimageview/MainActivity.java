15b857463fb2a8b5ce7c05c8e8c020ecd2b04b8e
Hemendra-s <hsr15498@gmail.com>
2019-08-24 21:13:57 +0530

    initial commit

---
+++ app/src/main/java/com/example/hsr15/gallerytoimageview/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.hsr15.gallerytoimageview;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button selectImage;
+    ImageView imageView;
+
+
+    private int REQUEST_CODE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        selectImage = (Button) findViewById(R.id.select);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        selectImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+            }catch (IOException e ){
+                e.printStackTrace();
+            }
+        }
+    }
+}
